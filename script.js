d2bf8e2dc77ef57f97076f4483d7f7b376b0f736
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2021-10-02 20:47:36 -0300

    Add files

---
+++ script.js
@@ -0,0 +1,17 @@
+function mudaModo()
+{
+  document.body.classList.toggle("dark");
+  // document.getElementsByClassName("codepen").(data-theme-id).value = "dark";
+  // document.querySelector(".codepen").setAttribute("data-theme-id", "dark");
+}
+
+
+
+
+
+
+
+
+
+// document.getElementsByClassName("codepen").(data-theme-id) = "dark";
+// document.getElementsByClassName("codepen") = '<p class="codepen" data-height="300" data-theme-id="light" data-default-tab="result" data-slug-hash="RwggyLd" data-user="mathgurgel" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"><span>See the Pen <a href="https://codepen.io/mathgurgel/pen/RwggyLd">Average Calculator</a> by mathgurgel (<a href="https://codepen.io/mathgurgel">@mathgurgel</a>)on <a href="https://codepen.io">CodePen</a>.</span></p>';
